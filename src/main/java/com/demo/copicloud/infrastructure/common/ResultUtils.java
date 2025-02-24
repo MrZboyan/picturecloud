7770210e5437d41563f56c388fe25728d24f6ec7
Zangdibo <duanmun9811110@gmail.com>
2025-02-24 17:11:25 +0800

    优化

--- src/main/java/com/demo/copicloud/infrastructure/common/ResultUtils.java
+++ src/main/java/com/demo/copicloud/infrastructure/common/ResultUtils.java
@@ -1,6 +1,7 @@
 package com.demo.copicloud.infrastructure.common;
 
 import com.demo.copicloud.infrastructure.exception.ErrorCode;
+import org.apache.poi.ss.formula.functions.T;
 
 /**
  * 返回工具类 5
@@ -9,10 +10,6 @@ public class ResultUtils {
 
     /**
      * 成功
-     *
-     * @param data
-     * @param <T>
-     * @return
      */
     public static <T> BaseResponse<T> success(T data) {
         return new BaseResponse<>(0, data, "ok");
@@ -20,32 +17,22 @@ public class ResultUtils {
 
     /**
      * 失败
-     *
-     * @param errorCode
-     * @return
      */
-    public static BaseResponse error(ErrorCode errorCode) {
+    public static BaseResponse<T> error(ErrorCode errorCode) {
         return new BaseResponse<>(errorCode);
     }
 
     /**
      * 失败
-     *
-     * @param code
-     * @param message
-     * @return
      */
-    public static BaseResponse error(int code, String message) {
-        return new BaseResponse(code, null, message);
+    public static BaseResponse<T> error(int code, String message) {
+        return new BaseResponse<>(code, null, message);
     }
 
     /**
      * 失败
-     *
-     * @param errorCode
-     * @return
      */
-    public static BaseResponse error(ErrorCode errorCode, String message) {
-        return new BaseResponse(errorCode.getCode(), null, message);
+    public static BaseResponse<T> error(ErrorCode errorCode, String message) {
+        return new BaseResponse<>(errorCode.getCode(), null, message);
     }
 }
